5222f7d68a0c342f1c40e1cdde315615c8773c47
leiboT <leibo_369@163.com>
2017-10-10 11:20:19 +0800

    add ajax loading

--- js/cater/cater.js
+++ js/cater/cater.js
@@ -1,6 +1,7 @@
 $(function(){
     //异步再封装
     function customAjax(url,data,fn){
+        //alert(JSON.stringify(arguments));
         $.ajax({
             type:"post",
             url:url,
@@ -8,7 +9,13 @@ $(function(){
             dataType:"json",
             success:fn,
             error:function(error){
-                console.log(error)
+                //console.log(error)
+            },
+            beforeSend: function(){
+                $('body').append('<div class="loadingWrap"></div>');
+            },
+            complete: function(){
+                $(".loadingWrap").remove();
             }
         })
     }
