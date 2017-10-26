5e89b8977bed0d7a6d6e2a085eb550b71671b93a
leiboT <leibo_369@163.com>
2017-10-26 17:41:01 +0800

    delete paging handle

--- js/myJs/myShops.js
+++ js/myJs/myShops.js
@@ -5,58 +5,30 @@ $(function(){
     var pop=$("#pop");
     var p=$("#pop p");
     var closeBtn=$(".pop-box>span").length?$(".pop-box>span"):$("#pop>div>div");
-    //提示弹出框处理函数
-    //function  reminderDeal(txt){
-    //    p.html(txt);
-    //    pop.addClass("pop-show");
-    //}
-    //异步再封装
-    //function customAjax(url,data,fn){
-    //    //alert(JSON.stringify(arguments));
-    //    $.ajax({
-    //        type:"post",
-    //        url:url,
-    //        data:data,
-    //        dataType:"json",
-    //        success:fn,
-    //        error:function(error){
-    //            //console.log(error)
-    //        },
-    //        beforeSend: function(){
-    //            $('body').append('<div class="loadingWrap"></div>');
-    //        },
-    //        complete: function(){
-    //            $(".loadingWrap").remove();
-    //        }
-    //    })
-    //}
-    //跳转
-    //function jump(url,t){
-    //    setTimeout(function(){
-    //        location.href=url
-    //    },t)
-    //}
     //我的推广页处理函数
     function myPromotion(){
-        var loadMore=$("#pro-list li.loadMore");
-        var index=1;
-        var pg;
+        var proList = $("#pro-list");
+        //var loadMore=$("#pro-list li.loadMore");
+        //var index=1;
+        //var pg;
 
         function promotionLoad(uid,page){
             $.customAjax(
                 "http://api.qianjiantech.com/v1/myBusiness",
-                {user_id:uid,page:page,state_code:sessionStorage.getItem("stateCode")},
+                {user_id:uid,
+                    //page:page,
+                    state_code:sessionStorage.getItem("stateCode")},
                 function(result){
                     //console.log(result);
                     var code=result.code;
                     //console.log(code);
                     if(code==2000){
-                        pg=result.page;
+                        //pg=result.page;
                         //console.log(page);
                         var info=result.info;
                         //console.log(info);
                         var len=info.length;
-                        len<10?loadMore.html("<div>没有更多了</div>"):loadMore.html("<div>上拉加载更多..</div>");
+                        //len<10?loadMore.html("<div>没有更多了</div>"):loadMore.html("<div>上拉加载更多..</div>");
                         for(var i=0,html=""; i<len; i++){
                             //console.log(i);
                             html+=`
@@ -71,29 +43,31 @@ $(function(){
                     </li>
                     `;
                         }
-                        $(loadMore).before(html);
+                        //$(loadMore).before(html);
+                        proList.html(html);
                     }else if(code==2001){
-                        loadMore.html("<div>您还没有商家</div>")
+                        //loadMore.html("<div>您还没有商家</div>");
+                        proList.html("<li class='noData'>您还没有商家</li>")
                     }else if(code==9000){
                         $.loginOtherDevice(p,pop,closeBtn,"../loginRegisterHTML/login.html")
                     }
                 }
             );
         }
-        promotionLoad(uid,index);
+        promotionLoad(uid);
 
         //下拉加载更多
-        $('body').scroll(function () {
-            //console.log(123);
-            if ($(this).scrollTop() + $(this).height() >= $("#pro-list").height()+50) {
-                //console.log("哦哦,到底了.");
-                index+=1;
-                index<=pg?loadMore.html("<div>加载中...</div>"):loadMore.html("<div>没有更多了</div>");
-                setTimeout(function(){
-                    index<=pg&&promotionLoad(uid,index);
-                },1000);
-            }
-        });
+        //$('body').scroll(function () {
+        //    //console.log(123);
+        //    if ($(this).scrollTop() + $(this).height() >= $("#pro-list").height()+50) {
+        //        //console.log("哦哦,到底了.");
+        //        index+=1;
+        //        index<=pg?loadMore.html("<div>加载中...</div>"):loadMore.html("<div>没有更多了</div>");
+        //        setTimeout(function(){
+        //            index<=pg&&promotionLoad(uid,index);
+        //        },1000);
+        //    }
+        //});
     }
     if(uid)
         myPromotion();
