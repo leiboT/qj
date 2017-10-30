9bc45c5ac14abb9277cbb9ea04e6dd49ec5ca61c
leiboT <leibo_369@163.com>
2017-10-30 10:22:30 +0800

    rewrite Zepto extend

--- js/base.js
+++ js/base.js
@@ -1,6 +1,6 @@
-(function() {
+(function($) {
     //会话储存
-    var sessionControl = function(){
+    $.sessionControl = function(){
         return {
             uid:sessionStorage.getItem("uid"),
             stateCode:sessionStorage.getItem("stateCode"),
@@ -8,7 +8,7 @@
         }
     };
     //异步
-    var customAjax = function(url,data,fn){
+    $.customAjax = function(url,data,fn){
         $.ajax({
             type:"post",
             url:url,
@@ -28,7 +28,7 @@
         })
     };
     //图片加载完清除占位图
-    var clearPlaceholderShape = function(){
+    $.clearPlaceholderShape = function(){
         $(".ui-fb").each(function(k,v){
             $(v).load(function(){
                 $(this).parent().removeClass("ui-lz");
@@ -36,7 +36,7 @@
         });
     };
     //判断轮播图片加载完成
-    var imgLoadingEnd = function(boxSelector,fnc,loadingBox){
+    $.imgLoadingEnd = function(boxSelector,fnc,loadingBox){
         var imgList = $(boxSelector).find('img');
         var callback = function(){
             fnc();
@@ -55,18 +55,18 @@
         });
     };
     //跳转
-    var jump = function(url,t){
+    $.jump = function(url,t){
         setTimeout(function(){
             location.href=url
         },t)
     };
     //提示弹出框处理函数
-    var reminderDeal = function(p,pop,txt){
+    $.reminderDeal = function(p,pop,txt){
         p.html(txt);
         pop.addClass("pop-show");
     };
     //请先登录处理函数
-    var pleaseLogin = function(p,pop,closeBtn,path){
+    $.pleaseLogin = function(p,pop,closeBtn,path){
         reminderDeal(p,pop,"请先登录!");
         closeBtn.html("进入登录页");
         closeBtn.on("click",function(){
@@ -74,14 +74,14 @@
         });
     };
     //已在其他设备登录
-    var loginOtherDevice = function(p,pop,closeBtn,path){
+    $.loginOtherDevice = function(p,pop,closeBtn,path){
         reminderDeal(p,pop,"你已在其他设备登录!");
         closeBtn.html("即将进入登录页").unbind("click");
         jump(path,1500);
         sessionStorage.clear();
     };
     //点击其他区域关闭弹框
-    var elseClosePop = function(pop){
+    $.elseClosePop = function(pop){
         pop.mouseup(function(e){
             var _con = $('.pop-box');
             if(_con != e.target && _con.has(e.target).length === 0){
@@ -90,7 +90,7 @@
         });
     };
     //服务器端图片转base64
-    var imgToBase64 = function(url){
+    $.imgToBase64 = function(url){
         var canvas = document.createElement('CANVAS'),
             ctx = canvas.getContext('2d'),
             img = new Image();
@@ -105,16 +105,16 @@
         };
     };
     //如果使用的是zepto，就添加扩展函数
-    if(Zepto){
-        $.customAjax = customAjax;
-        $.clearPlaceholderShape=clearPlaceholderShape;
-        $.imgLoadingEnd=imgLoadingEnd;
-        $.jump=jump;
-        $.reminderDeal=reminderDeal;
-        $.loginOtherDevice=loginOtherDevice;
-        $.pleaseLogin=pleaseLogin;
-        //$.sessionControl=sessionControl;
-        $.elseClosePop=elseClosePop;
-        $.imgToBase64=imgToBase64;
-    }
-})();
+    //if(Zepto){
+    //    $.customAjax = customAjax;
+    //    $.clearPlaceholderShape=clearPlaceholderShape;
+    //    $.imgLoadingEnd=imgLoadingEnd;
+    //    $.jump=jump;
+    //    $.reminderDeal=reminderDeal;
+    //    $.loginOtherDevice=loginOtherDevice;
+    //    $.pleaseLogin=pleaseLogin;
+    //    //$.sessionControl=sessionControl;
+    //    $.elseClosePop=elseClosePop;
+    //    $.imgToBase64=imgToBase64;
+    //}
+})(Zepto);
